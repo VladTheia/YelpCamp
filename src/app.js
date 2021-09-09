1aa76bc2e72ee0a6531fb0dbffb5e0d9e2ca746c
Theia Vlad <theia@Theias-MacBook-Air.local>
2021-09-09 11:39:05 +0300

    dockerize the app

--- src/app.js
+++ src/app.js
@@ -18,15 +18,8 @@ const commentRoutes = require('./routes/comments'),
       campgroundRoutes = require('./routes/campgrounds'),
       indexRoutes = require('./routes/index');
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true })
-// mongoose.connect(process.env.DATABASEURL, { 
-//     useNewUrlParser: true,
-//     useCreateIndex: true 
-// }).then(() => {
-//     console.log('Connected to DB')
-// }).catch(err => {
-//     console.log(err.message)
-// })
+mongoose.connect("mongodb://mongo/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true })
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
